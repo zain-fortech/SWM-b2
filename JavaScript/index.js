2d50e915f26166a5a1405aff450f09839dccfee3
Zain Ali <zain.fortech@gmail.com>
2026-08-22 15:19:22 +0500

    JS

---
+++ JavaScript/index.js
@@ -0,0 +1,23 @@
+const _age = 25; // integer
+
+const reducedAge = _age - 2;
+
+const multipliedAge = _age * 5;
+
+const dividedAge = _age / 1.5;
+
+const remainderAge = _age % 3;
+
+const sum = 3 + 4;
+
+let city = "Gujranwala"; // string
+
+const startingString = "My city is ";
+
+const message = "3" + 4;
+
+var isMale = true; // boolean
+
+city = "Sialkot";
+
+alert(message);
